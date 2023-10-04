50013e785e6500154eb868ff53bf402727f8e4cc
Memo9494 <104597078+Memo9494@users.noreply.github.com>
2023-10-04 10:08:47 -0600

    Integracion Opencv y facerecon

--- video_stream/views.py
+++ video_stream/views.py
@@ -51,6 +51,26 @@ class VideoCamera(object):
         # print(self.f_circulo_encodings)
         self.video = cv2.VideoCapture(0)
         
+        print("hola")
+        self.f_circulo_encodings = []
+        self.f_circulo_names = []
+        circuloPath = os.path.dirname(os.path.abspath(__file__))
+        circuloPath = circuloPath + '\\encodings.txt'
+        circuloPath = circuloPath.replace("\\","/")
+        with open(circuloPath, "r") as file:
+            for line in file.readlines():
+                # Separar el nombre y los encodings usando la coma como delimitador
+                parts = line.strip().split(",")
+                name = parts[0]
+                encoding_str = parts[1:]
+
+                # Convertir los encodings de strings a flotantes
+                encoding = [float(x) for x in encoding_str]
+
+                # Agregar el nombre y los encodings a las listas correspondientes
+                self.f_circulo_names.append(name)
+                self.f_circulo_encodings.append(encoding)
+
 
 
         (self.grabbed,self.frame) = self.video.read()
@@ -62,26 +82,26 @@ class VideoCamera(object):
         image = self.frame
         # print(self.f_circulo_names)
         
-        # f_data_locations = face_recognition.face_locations(image) # Obtiene las coordenadas del rostro en la imagen
-        # if f_data_locations != []:                                # Si se detecta un rostro
-        #     print("[PROCESO] Rostro detectado")
-        #     face_names = []
-        #     f_frame_codings = face_recognition.face_encodings(image,f_data_locations)        # Obtenemos las características del rostro encontrado
-        #     for face_encoding, (top, right, bottom, left) in zip(f_frame_codings, f_data_locations):                                            # Comparamos el rostro encontrado con los rostros conocidos
-        #         matches = face_recognition.compare_faces(self.f_circulo_encodings, face_encoding)
-        #         print("Matches: ", matches)
-        #         if True in matches:                                                          # Si se reconoce el rostro
-        #             index = matches.index(True)
-        #             name = self.f_circulo_names[index]
-        #             face_names.append(name)
-        #         else:
-        #             name = "Desconocido"
-        #         cv2.rectangle(image, (left, top), (right, bottom), (0,255,0), 2)
-        #         cv2.rectangle(image, (left, bottom -10), (right, bottom), (0,255,0), cv2.FILLED)
-        #         font = cv2.FONT_HERSHEY_DUPLEX
-        #         cv2.putText(image, name, (left +3, bottom -3), font, 0.4, (255,255,255), 1)
-        # else:
-        #     print("[ALERTA] No se detecto un rostro")
+        f_data_locations = face_recognition.face_locations(image) # Obtiene las coordenadas del rostro en la imagen
+        if f_data_locations != []:                                # Si se detecta un rostro
+            print("[PROCESO] Rostro detectado")
+            face_names = []
+            f_frame_codings = face_recognition.face_encodings(image,f_data_locations)        # Obtenemos las características del rostro encontrado
+            for face_encoding, (top, right, bottom, left) in zip(f_frame_codings, f_data_locations):                                            # Comparamos el rostro encontrado con los rostros conocidos
+                matches = face_recognition.compare_faces(self.f_circulo_encodings, face_encoding)
+                print("Matches: ", matches)
+                if True in matches:                                                          # Si se reconoce el rostro
+                    index = matches.index(True)
+                    name = self.f_circulo_names[index]
+                    face_names.append(name)
+                else:
+                    name = "Desconocido"
+                cv2.rectangle(image, (left, top), (right, bottom), (0,255,0), 2)
+                cv2.rectangle(image, (left, bottom -10), (right, bottom), (0,255,0), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(image, name, (left +3, bottom -3), font, 0.4, (255,255,255), 1)
+        else:
+            print("[ALERTA] No se detecto un rostro")
 
 
         _, jpeg = cv2.imencode('.jpg', image)
